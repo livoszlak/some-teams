bdc9651cf9fcb654f734aaf08b9b0f4d11935f9e
Liv <110914989+livoszlak@users.noreply.github.com>
2023-10-03 14:34:37 +0200

    Fixed a syntax faux pas

--- print-data.php
+++ print-data.php
@@ -7,19 +7,19 @@ foreach ($teams as $teamName => $teamData) : ?>
         <!--Using ucwords() for the following table entries in case any data is entered without proper capitalization.-->
         <td><?= ucwords($teamData['league']); ?></td>
         <!--The following if statement makes sure either to print year, if year exists in the array, and "Never" if the team has never won a championship (aww).-->
-        <?php if ($teamData['last-time-champions'] !== null) : ?>
+        <?php if ($teamData['last-time-champions'] !== null) { ?>
             <td><?= $teamData['last-time-champions']; ?></td>
-        <?php else : ?>
+        <?php } else { ?>
             <td><?= "Never" ?></td>
-        <?php endif; ?>
+        <?php } ?>
         <td><?= ucwords($teamData['city']); ?></td>
         <!--The following if statement prints the team's nickname if there is a 'nickname' key in their array, and "None" if there isn't.-->
-        <?php if (array_key_exists('nickname', $teamData)) : ?>
+        <?php if (array_key_exists('nickname', $teamData)) { ?>
             <td><?= ucwords($teamData['nickname']); ?></td>
-        <?php else : ?>
+        <?php } else { ?>
             <td><?= "None"; ?></td>
-        <?php endif; ?>
+        <?php } ?>
         <!--Using strtolower() and printing the substring of the url excluding "https://www.", and utilizing the whole url for the anchor href.-->
         <td><a href="<?= $teamData['url']; ?>"><?= strtolower(substr($teamData['url'], 12, -1)); ?></td>
     </tr>
-<?php endforeach;
+<?php endforeach; ?>
